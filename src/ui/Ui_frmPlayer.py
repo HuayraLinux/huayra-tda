ed7ea633ef19cdb78b7f06ec1caa5ab47b9d3bc5
Miguel Angel Garcia <miguel.garcia@gmail.com>
2014-03-12 00:26:20 -0300

    Mejoras esteticas

--- src/ui/Ui_frmPlayer.py
+++ src/ui/Ui_frmPlayer.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'player.ui'
 #
-# Created: Wed Mar  5 15:10:11 2014
+# Created: Wed Mar 12 00:25:42 2014
 #      by: PyQt4 UI code generator 4.9.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -17,49 +17,99 @@ except AttributeError:
 class Ui_frmPlayer(object):
     def setupUi(self, frmPlayer):
         frmPlayer.setObjectName(_fromUtf8("frmPlayer"))
-        frmPlayer.resize(494, 319)
+        frmPlayer.setEnabled(True)
+        frmPlayer.resize(665, 496)
+        frmPlayer.setMaximumSize(QtCore.QSize(100000, 100000))
+        frmPlayer.setAutoFillBackground(False)
+        frmPlayer.setStyleSheet(_fromUtf8("QWidget#frmPlayer{\n"
+"background-color: QLinearGradient( x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #f00000, stop: 0.3 #00f000, stop: 1 #0000f0);\n"
+"\n"
+"}\n"
+"\n"
+"#layoutVideo{\n"
+"    border: 1px solid #e9e9e9;\n"
+"    border-radius:6px;\n"
+"}\n"
+"\n"
+"#sldVolume::groove:horizontal {\n"
+"     border: 1px solid #999999;\n"
+"     height: 8px; /* the groove expands to the size of the slider by default. by giving it a height, it has a fixed size */\n"
+"     background: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #B1B1B1, stop:1 #c4c4c4);\n"
+"     margin: 2px 0;\n"
+"    border-radius: 2px;\n"
+" }\n"
+"\n"
+"#sldVolume::handle:horizontal {\n"
+"     background: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 #4bd0d3, stop:0.2 #3b9b9e, stop:0.8 #4bd0d3, stop:1 #2b4040);\n"
+"     border: 1px solid #5c5c5c;\n"
+"     width: 18px;\n"
+"     margin: -2px 0; /* handle is placed by default on the contents rect of the groove. Expand outside the groove */\n"
+"     border-radius: 5px;\n"
+" }\n"
+""))
         self.verticalLayout = QtGui.QVBoxLayout(frmPlayer)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
         self.layoutVideo = QtGui.QHBoxLayout()
+        self.layoutVideo.setContentsMargins(-1, 0, -1, -1)
         self.layoutVideo.setObjectName(_fromUtf8("layoutVideo"))
-        spacerItem = QtGui.QSpacerItem(20, 200, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        spacerItem = QtGui.QSpacerItem(0, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
         self.layoutVideo.addItem(spacerItem)
         self.verticalLayout.addLayout(self.layoutVideo)
         self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.btnChannelDown = QtGui.QPushButton(frmPlayer)
-        self.btnChannelDown.setStyleSheet(_fromUtf8("color: rgb(125, 60, 255);"))
-        self.btnChannelDown.setObjectName(_fromUtf8("btnChannelDown"))
-        self.horizontalLayout.addWidget(self.btnChannelDown)
         self.btnChannelUp = QtGui.QPushButton(frmPlayer)
+        self.btnChannelUp.setMaximumSize(QtCore.QSize(32, 32))
+        self.btnChannelUp.setText(_fromUtf8(""))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8("../../../../../../usr/share/icons/huayra-limbo/scalable/acciones/up.svg")), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        self.btnChannelUp.setIcon(icon)
         self.btnChannelUp.setObjectName(_fromUtf8("btnChannelUp"))
         self.horizontalLayout.addWidget(self.btnChannelUp)
+        self.btnChannelDown = QtGui.QPushButton(frmPlayer)
+        self.btnChannelDown.setMaximumSize(QtCore.QSize(32, 32))
+        self.btnChannelDown.setStyleSheet(_fromUtf8("color: rgb(125, 60, 255);"))
+        self.btnChannelDown.setText(_fromUtf8(""))
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(_fromUtf8("../../../../../../usr/share/icons/huayra-limbo/scalable/acciones/down.svg")), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        self.btnChannelDown.setIcon(icon1)
+        self.btnChannelDown.setObjectName(_fromUtf8("btnChannelDown"))
+        self.horizontalLayout.addWidget(self.btnChannelDown)
         self.sldVolume = QtGui.QSlider(frmPlayer)
         self.sldVolume.setOrientation(QtCore.Qt.Horizontal)
         self.sldVolume.setObjectName(_fromUtf8("sldVolume"))
         self.horizontalLayout.addWidget(self.sldVolume)
-        spacerItem1 = QtGui.QSpacerItem(50, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem1)
         self.btnShowChannelsList = QtGui.QPushButton(frmPlayer)
+        self.btnShowChannelsList.setMaximumSize(QtCore.QSize(30, 32))
+        self.btnShowChannelsList.setText(_fromUtf8(""))
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap(_fromUtf8("../../../../../../usr/share/icons/huayra-limbo/scalable/acciones/top.svg")), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        self.btnShowChannelsList.setIcon(icon2)
         self.btnShowChannelsList.setObjectName(_fromUtf8("btnShowChannelsList"))
         self.horizontalLayout.addWidget(self.btnShowChannelsList)
-        spacerItem2 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem2)
         self.btnFullScreen = QtGui.QPushButton(frmPlayer)
+        self.btnFullScreen.setMaximumSize(QtCore.QSize(32, 32))
+        self.btnFullScreen.setText(_fromUtf8(""))
+        icon3 = QtGui.QIcon()
+        icon3.addPixmap(QtGui.QPixmap(_fromUtf8("../../../../../../usr/share/icons/huayra-limbo/scalable/acciones/view-fullscreen.svg")), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        self.btnFullScreen.setIcon(icon3)
         self.btnFullScreen.setObjectName(_fromUtf8("btnFullScreen"))
         self.horizontalLayout.addWidget(self.btnFullScreen)
         self.verticalLayout.addLayout(self.horizontalLayout)
-        self.listView = QtGui.QListView(frmPlayer)
-        self.listView.setObjectName(_fromUtf8("listView"))
-        self.verticalLayout.addWidget(self.listView)
+        self.listViewChannels = QtGui.QListView(frmPlayer)
+        self.listViewChannels.setEnabled(True)
+        self.listViewChannels.setMaximumSize(QtCore.QSize(16777215, 200))
+        self.listViewChannels.setAutoFillBackground(False)
+        self.listViewChannels.setObjectName(_fromUtf8("listViewChannels"))
+        self.verticalLayout.addWidget(self.listViewChannels)
 
         self.retranslateUi(frmPlayer)
         QtCore.QMetaObject.connectSlotsByName(frmPlayer)
 
     def retranslateUi(self, frmPlayer):
-        frmPlayer.setWindowTitle(QtGui.QApplication.translate("frmPlayer", "Form", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnChannelDown.setText(QtGui.QApplication.translate("frmPlayer", "-", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnChannelUp.setText(QtGui.QApplication.translate("frmPlayer", "+", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnShowChannelsList.setText(QtGui.QApplication.translate("frmPlayer", "\\/", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnFullScreen.setText(QtGui.QApplication.translate("frmPlayer", "Full", None, QtGui.QApplication.UnicodeUTF8))
+        frmPlayer.setWindowTitle(QtGui.QApplication.translate("frmPlayer", "Huayra - Television Digital Abierta ", None, QtGui.QApplication.UnicodeUTF8))
+        frmPlayer.setToolTip(QtGui.QApplication.translate("frmPlayer", "Visor de TDA Huayra", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnChannelUp.setToolTip(QtGui.QApplication.translate("frmPlayer", "Canal Arriba", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnChannelDown.setToolTip(QtGui.QApplication.translate("frmPlayer", "Canal Abajo", None, QtGui.QApplication.UnicodeUTF8))
+        self.sldVolume.setToolTip(QtGui.QApplication.translate("frmPlayer", "Control de volumen", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnFullScreen.setToolTip(QtGui.QApplication.translate("frmPlayer", "Pantalla Completa", None, QtGui.QApplication.UnicodeUTF8))
 
--- src/ui/WidgetPlayer.py
+++ src/ui/WidgetPlayer.py
@@ -9,7 +9,10 @@ class WidgetPlayer(QtGui.QWidget):
         self.ui.setupUi(self)
         self.createVideoFrame()
         self.ui.layoutVideo.addWidget(self.videoframe)
-
+        self.connect(self.ui.btnShowChannelsList, QtCore.SIGNAL("clicked()"), self.showHideChannelsList)
+        self.connect(self.ui.btnChannelUp, QtCore.SIGNAL("clicked()"), self.channelUp)
+        self.connect(self.ui.btnChannelDown, QtCore.SIGNAL("clicked()"), self.channelDown)
+                
     def createVideoFrame(self):
         if sys.platform == "darwin": # for MacOS
             self.videoframe = QtGui.QMacCocoaViewContainer(0)
@@ -20,4 +23,27 @@ class WidgetPlayer(QtGui.QWidget):
                                QtGui.QColor(0,0,0))
         self.videoframe.setPalette(self.palette)
         self.videoframe.setAutoFillBackground(True)
-        
+
+    def showHideChannelsList(self):
+        if self.ui.listViewChannels.isHidden():
+            self.ui.listViewChannels.show()
+        else:
+            self.ui.listViewChannels.hide()
+
+    def channelUp(self):
+        self.player.gotoChannelUp()
+
+    def channelDown(self):
+        self.player.gotoChannelDown()
+
+    def paintEvent(self, pe):
+        opt = QtGui.QStyleOption()
+        opt.init(self)
+        p = QtGui.QPainter(self)
+        s = self.style()
+        s.drawPrimitive(QtGui.QStyle.PE_Widget, opt, p, self)
+        #  QStyleOption o;                                                                                                                                                                  
+#  o.initFrom(this);                                                                                                                                                                
+#  QPainter p(this);                                                                                                                                                                
+#  style()->drawPrimitive(QStyle::PE_Widget, &o, &p, this);                                                                                                                         
+#};
