c0e40995036ac47396abbde4700a91d218b3ad25
Miguel Angel Garcia <miguel.garcia@gmail.com>
2014-03-18 16:54:12 -0300

    remove tooltip

--- src/ui/Ui_frmAbout.py
+++ src/ui/Ui_frmAbout.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'about.ui'
 #
-# Created: Mon Mar 17 14:52:17 2014
+# Created: Tue Mar 18 16:53:46 2014
 #      by: PyQt4 UI code generator 4.9.3
 #
 # WARNING! All changes made in this file will be lost!
