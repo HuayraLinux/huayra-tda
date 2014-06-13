f215164bac899a1c51e57c45444ac8a0f9eec6b5
Ignacio Juan Martín Benedetti <tranceway@gmail.com>
2014-06-13 12:52:50 -0300

    Prueba de debug

--- src/config.py
+++ src/config.py
@@ -3,6 +3,13 @@
 import os
 from channel import Channel
 
+# Canales sin nombres
+CANALES = {
+    '[e741]': 'TV Pública HD',
+    '[e758]': 'TV Pública SD',
+    '[e742]': 'TV Pública Móvil',
+}
+
 class Config:
     CHANNELS_FILE = '.channels.conf'
 
@@ -15,9 +22,15 @@ class Config:
             return
         f = open(path, 'r')
         for line in f.readlines():
-            params = line.split(":")            
+            params = line.split(":")
+
+            print params[0].strip(), params[1].strip(), params[-1:][0].strip()
+
+
             channel = Channel(params[0].strip(), params[1].strip(), params[-1:][0].strip())
             guide.addChannel(channel)
+
+        raise
 
     def save(self, txt):
         path = self.path + Config.CHANNELS_FILE
