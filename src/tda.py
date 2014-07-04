3614ebfb14652feb48bf6e794a8d96b3f6558a1c
Ignacio Juan Martín Benedetti <tranceway@gmail.com>
2014-07-04 13:57:20 -0300

    Panel de Video y Panel de control con botones básicos. Barra de estado simple.

--- src/tda.py
+++ src/tda.py
@@ -3,6 +3,8 @@
 
 import wx
 
+
+
 class MainFrame(wx.Frame):
     def __init__(self):
         super(MainFrame, self).__init__(
@@ -11,58 +13,66 @@ class MainFrame(wx.Frame):
             title=u'Huayra TDA',
         )
 
+        self.status_bar = self.CreateStatusBar()
+
         # Menú archivo
         file_menu = wx.Menu()
-        file_menu.Append(1, u'Escanear canales')
+        file_menu.Append(id=-1, text=u'Escanear canales')
         file_menu.AppendSeparator()
         file_menu.Append(wx.ID_EXIT)
 
-        # Menú opciones
+            # Submenú desentrelazado
         opt_deinterlace_menu = wx.Menu()
         opt_deinterlace_menu.AppendRadioItem(id=-1, text=u'Ninguno')
         opt_deinterlace_menu.AppendRadioItem(id=-1, text=u'Blend')
         opt_deinterlace_menu.AppendRadioItem(id=-1, text=u'Linear')
         opt_deinterlace_menu.AppendRadioItem(id=-1, text=u'X')
 
+            # Submenú aspecto
         opt_aspect_menu = wx.Menu()
         opt_aspect_menu.AppendRadioItem(id=-1, text=u'Ninguno')
         opt_aspect_menu.AppendRadioItem(id=-1, text=u'4:3')
         opt_aspect_menu.AppendRadioItem(id=-1, text=u'16:9')
         opt_aspect_menu.AppendRadioItem(id=-1, text=u'16:10')
 
+        # Menú opciones
         options_menu = wx.Menu()
         options_menu.AppendSubMenu(opt_deinterlace_menu, 'Desentrelazado')
         options_menu.AppendSubMenu(opt_aspect_menu, 'Aspecto')
 
+        # Asignación de Menú
         menu_bar = wx.MenuBar()
         menu_bar.Append(file_menu, u'Archivo')
         menu_bar.Append(options_menu, u'Opciones')
-
         self.SetMenuBar(menu_bar)
 
-        # Paneles
-        # Panel de reproductor
-        self.video_panel = wx.Panel(parent=self, id=-1)
-        self.video_panel.SetBackgroundColour(wx.RED)
+        # Panel de video
+        self.panel_video = wx.Panel(parent=self)
+        self.panel_video.SetBackgroundColour(wx.BLACK)
 
         # Panel de control
-        control_panel = wx.Panel(parent=self)
-        pause = wx.Button(control_panel, label="Pause")
+        panel_control = wx.Panel(parent=self)
+        panel_control.SetBackgroundColour(wx.RED)
 
-        # Sizers
+        # Botones de control
+        channel_up = wx.Button(parent=panel_control, label=u'Subir canal')
+        channel_down = wx.Button(parent=panel_control, label=u'Bajar canal')
+        volume_mute = wx.Button(parent=panel_control, label=u'Silencio')
+        take_picture = wx.Button(parent=panel_control, label=u'Foto')
+
+        szr_control = wx.BoxSizer(wx.HORIZONTAL)
+        szr_control.Add(channel_down, flag=wx.RIGHT, border=2)
+        szr_control.Add(channel_up, flag=wx.RIGHT, border=2)
+        szr_control.Add(volume_mute, flag=wx.RIGHT, border=2)
+        szr_control.Add(take_picture, flag=wx.RIGHT, border=2)
+        panel_control.SetSizer(szr_control)
+
         sizer = wx.BoxSizer(wx.VERTICAL)
-        box_control = wx.BoxSizer(wx.HORIZONTAL)
-
-        box_control.Add(control_panel)
-
-        sizer.Add(self.video_panel, flag=wx.EXPAND)
-        sizer.Add(box_control, flag=wx.EXPAND)
-
+        sizer.Add(self.panel_video, 1, flag=wx.EXPAND)
+        sizer.Add(panel_control, flag=wx.EXPAND|wx.TOP, border=2)
         self.SetSizer(sizer)
-        self.SetMinSize((350, 300))
 
-        # Bindeos
-        self.Bind(wx.EVT_MENU, self.OnExit, id=wx.ID_EXIT)
+
 
         self.Center()
 
