1a74af9b24000ec148bc18905316cedf83c94211
Ignacio Juan Martín Benedetti <tranceway@gmail.com>
2014-08-20 12:47:47 -0300

    Templates de HTML y mejoras en Scan

--- src/views/scan.py
+++ src/views/scan.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import wx
+import wx.html
+import wx.lib.scrolledpanel as scrolled
 
 
 class ChannelScan(wx.Frame):
@@ -11,15 +13,42 @@ class ChannelScan(wx.Frame):
             title=u'Escaneo de canales'
         )
 
+        self._pref= wx.GetApp().preferences
+
         panel = wx.Panel(parent=self)
         panel.SetBackgroundColour(wx.BLACK)
 
         self.btn_scan = wx.Button(parent=panel, label=u'Escanear')
-        self.btn_continue = wx.Button(parent, label=u'Continuar')
+        self.btn_scan_cancel = wx.Button(parent=panel, label=u'Cancelar')
+        self.btn_close = wx.Button(parent=panel, label=u'Cerrar')
+        self.btn_close.Bind(wx.EVT_BUTTON, self.OnClose)
 
         szr_buttons = wx.BoxSizer(wx.HORIZONTAL)
         szr_buttons.Add(self.btn_scan, flag=wx.RIGHT, border=2)
-        szr_buttons.Add(self.btn_continue, flag=wx.RIGHT, border=2)
+        szr_buttons.Add(self.btn_scan_cancel, flag=wx.RIGHT, border=2)
+        szr_buttons.Add(self.btn_close, flag=wx.RIGHT, border=2)
+        panel.SetSizer(szr_buttons)
 
-        self.SetSizer(szr_buttons)
+        # --
+        self.messages = wx.html.HtmlWindow(parent=self)
+        self.messages.SetPage(self._pref.load_html('scan_start'))
+
+        # --
+
+        self.progress_txt = wx.StaticText(parent=self, label="Escanendo %s")
+        self.gauge = wx.StaticText(parent=self, label="Escanendo %s")
+        # --
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(wx.StaticText('Escanear canales'))
+        sizer.Add(self.messages, 1, flag=wx.EXPAND)
+        sizer.Add(panel)
+
+        self.SetSizer(sizer)
+
+        self.SetMinSize((450, 300))
+        self.Center()
+
+    def OnClose(self, evt):
+        self.Close()
 
